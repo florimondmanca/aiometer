f927ef7a8513e1ad18b018650be0f50678d8c133
florimondmanca <florimond.manca@gmail.com>
2020-03-21 11:26:21 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,44 @@
+import re
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+
+def get_version(package: str) -> str:
+    version = (Path("src") / package / "__version__.py").read_text()
+    match = re.search("__version__ = ['\"]([^'\"]+)['\"]", version)
+    assert match is not None
+    return match.group(1)
+
+
+def get_long_description() -> str:
+    with open("README.md", encoding="utf8") as readme:
+        with open("CHANGELOG.md", encoding="utf8") as changelog:
+            return readme.read() + "\n\n" + changelog.read()
+
+
+setup(
+    name="aiometer",
+    version=get_version("aiometer"),
+    description="Concurrency scheduling library for asyncio",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="http://github.com/florimondmanca/msgpack-asgi",
+    author="Florimond Manca",
+    author_email="florimond.manca@gmail.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    python_requires=">=3.7",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
---
+++ tests/test_temp.py
@@ -0,0 +1,2 @@
+def test_remove_me() -> None:
+    import aiometer  # noqa: 401
