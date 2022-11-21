a641a54f8d05072a89c542eb50ff0edbeca52129
mxrch <17338428+mxrch@users.noreply.github.com>
2022-11-21 07:26:30 +0100

    Fix edge case in list from indexed dict (#35)

--- src/aiometer/_impl/utils.py
+++ src/aiometer/_impl/utils.py
@@ -9,7 +9,7 @@ def list_from_indexed_dict(dct: Mapping[int, T]) -> List[T]:
     """
     Given `{0: 'v_0', ..., n: 'v_N'}`, return `['v_0', ... 'v_n']`.
     """
-    return [dct[index] for index in range(max(dct) + 1)]
+    return [dct[index] for index in range(max(dct) + 1)] if dct else []
 
 
 def check_strictly_positive(name: str, value: float) -> None:
